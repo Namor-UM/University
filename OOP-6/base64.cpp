379e43ac13b35a17255ebc8627bea55cd64b4d5e
Namor-UM <roman-timerkhanov@mail.ru>
2022-02-03 15:29:19 +1000

    Перестройка задания под юнит-тесты

---
+++ OOP-6/base64.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Роман on 02.02.2022.
+//
+
+#include "base64.h"
---
+++ OOP-6/base64.h
@@ -0,0 +1,8 @@
+//
+// Created by Роман on 02.02.2022.
+//
+
+#ifndef OOP_6_BASE64_H
+#define OOP_6_BASE64_H
+
+#endif //OOP_6_BASE64_H
